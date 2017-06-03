d34050955488ae2b845d2276ea4f12b31e9a5432
Kaustubh Memane <memane.kaustubh@gmail.com>
2017-06-03 19:30:44 -0400

    Tutorial for people who wants to learn Kotlin
    Game Project in Kotlin

---
+++ src/Card.kt
@@ -0,0 +1,4 @@
+ /**
+ * Created by kmema on 6/3/2017.
+ */
+class Card(val value : Int,val suit: String,var faceUp: Boolean = false)
---
+++ src/app.kt
@@ -0,0 +1,6 @@
+/**
+ * Created by kmema on 6/3/2017.
+ */
+fun main(args: Array<String>): Unit{
+
+}
